2d679c612d01eea33d2e0fbfb6ce62ecbe4fd650
amratef1998 <amro14181998@gmail.com>
2024-02-05 02:41:44 +0200

    Add project files.

---
+++ ReadViewsAndCreateSheets/Command.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadViewsAndCreateSheets
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            try
+            {
+                XYZ placement = null;
+                int i = 1;
+                List<ViewsElevations> viewsElevationsList = new List<ViewsElevations>();
+                var Views = new FilteredElementCollector(doc).OfClass(typeof(View)).WhereElementIsNotElementType().Cast<View>().Where(x => x.ViewType.Equals(ViewType.FloorPlan) && x.CanBePrinted.Equals(true)).ToList();
+                foreach (var view in Views)
+                {
+                    var elev = view.GenLevel.Elevation;
+                    var viewName = view.Name;
+                    ViewsElevations viewsEle = new ViewsElevations(elev, viewName,view);
+                    viewsElevationsList.Add(viewsEle);
+                }
+                var sheet = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().Cast<ViewSheet>().FirstOrDefault();
+                uidoc.RequestViewChange(sheet);
+                placement = uidoc.Selection.PickPoint("Please Select Placement Point");
+                var titleBlock = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_TitleBlocks).WhereElementIsElementType().FirstOrDefault();
+                var orderedList = viewsElevationsList.OrderBy(e => e.Elevation).ToList();
+                Transaction tr = new Transaction(doc, "Create sheet and place view");
+                foreach (var viewEle in orderedList)
+                {
+                    tr.Start();
+                    var mysheet = ViewSheet.Create(doc, titleBlock.Id);
+                    tr.Commit();
+                    if (sheet != null)
+                    {
+                        tr.Start();
+                        mysheet.Name = viewEle.ViewName;
+                        mysheet.SheetNumber = $"A{i + 100}";
+                       
+                        tr.Commit() ;
+                        i++;
+                    }
+                   
+                   
+                    tr.Start();
+                    Viewport.Create(doc, mysheet.Id, viewEle.MyView.Id, placement);
+
+                    tr.Commit() ;
+
+                }
+                
+            
+            
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+
+               TaskDialog.Show("Warning",ex.Message);
+                return Result.Failed;
+            }
+        }
+
+    }
+}
---
+++ ReadViewsAndCreateSheets/ViewsElevations.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadViewsAndCreateSheets
+{
+    internal class ViewsElevations
+    {
+     
+
+        public double Elevation { get; set; }
+        public string ViewName { get; set; }
+        public View MyView { get; set; }
+        public ViewsElevations(double elevation, string viewName,View view)
+        {
+            Elevation = elevation;
+            ViewName = viewName;
+            MyView = view;
+        }
+    }
+}
